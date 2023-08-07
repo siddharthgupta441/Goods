e8355b98d472792acd44559549a8de19a219dcce
Dhananjay <sidharthgupta441@gmail.com>
2023-08-07 21:02:17 +0530

    uploading

---
+++ app/src/main/java/com/example/goods/chooseUser.java
@@ -0,0 +1,14 @@
+package com.example.goods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chooseUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_user);
+    }
+}
